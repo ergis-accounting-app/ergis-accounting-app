27f854accbba6b80e150c7c76a0ca319a79bf886
WolnyMikolaj <136742392+WolnyMikolaj@users.noreply.github.com>
2023-12-14 18:02:39 +0100

    Podstawowe serwerowe

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,8 @@
-console.log('test');
+const express = require('express');
+const app = express();
+
+//const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
